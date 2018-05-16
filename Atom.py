a55ba1eb172ab57c16ee4b47195e366b17ce6d40
HerpDerp <herpitehderpo@gmail.com>
2018-05-15 21:53:21 -0400

    initial commit

---
+++ Atom.py
@@ -0,0 +1,12 @@
+
+
+class atom():
+    
+    def __init__(self,particle_number,x,y,z):
+        
+        self.particle_number = particle_number
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    
